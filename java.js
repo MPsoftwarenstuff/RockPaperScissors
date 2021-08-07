94f87e62db46a4f7dbee04af94bf752abdfcbbd2
MPsoftwarenstuff <micahperkins0@gmail.com>
2021-08-07 13:33:17 -0400

    edits

---
+++ java.js
@@ -0,0 +1,30 @@
+let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+
+function playRound() {
+
+}
+
+function playCompTurn() {
+}
+
+function randomChoice() {
+	let randomNumber = Math.floor(Math.random() * 3)
+  	switch (randomNumber) {
+    		case 0:
+      			return 'ROCK'
+    		case 1:
+      			return 'PAPER'
+    		case 2:
+      			return 'SCISSORS'
+  }
+}
+function findWinner() {
+}
+
+function reset() {
+}
+
+
